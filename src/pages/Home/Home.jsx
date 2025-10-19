87f42bc69b89f694a148c6a31a3b7a081c0e4a40
João Marcelo de Oliveira Delfino <joaomarcelo318@outlook.com>
2025-10-19 14:23:57 -0300

    feat: Alteracoes home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar"
 import styles from "./Home.module.css"
+import Empilhadeiras from "../../assets/empilhadeiras.png"
 
 function Home({setShowLoginModal, setShowRegisterModal}){
 
     return(
         <div className={styles.container}>
             <Navbar setShowLoginModal={setShowLoginModal} setShowRegisterModal={setShowRegisterModal}/>
-            
+            <div className={styles.topContent}>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.title}>Serviços mecânicos e aluguel de empilhadeiras</h1>
+                    <h2 className="subtitle">Conserto de automóveis e aluguel de empilhadeiras</h2>
+                </div>
+                <div className={styles.imageContainer}>
+                    
+                </div>
+            </div>
         </div>
     );
 }
