041d103e3d69c58169574cb5b3c0b73cb2a9739a
Joao Marcelo <joaomarcelo318@outlook.com>
2025-12-01 01:55:05 -0300

    chore: Adequacoes para deploy

--- src/apiInterceptor.js
+++ src/apiInterceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://passig-empilhadeiras-backend.onrender.com/api",
   withCredentials: true
 });
 
